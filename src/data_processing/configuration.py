01baf2232ee82437df01414152eeea89cfa39f9f
shibin <shibintv@hotmail.com>
2019-02-20 00:30:57 -0600

    Adding configuration

--- src/data_processing/configuration.py
+++ src/data_processing/configuration.py
@@ -1,19 +1,24 @@
 
 import json
+import os
 class Configuration:
 
 
     def __init__(self,type=None):
+        dir_path =self.get_current_path()
+        conf_path = os.path.join(dir_path,'conf.json')
         if not type:
             type="gdrive"
         self.type = type
-        with open('conf.json') as f:
+        with open(conf_path,'r') as f:
             json.load(f)
             self.conf = f[type]
 
 
 
-
+    def get_current_path(self):
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        return dir_path
     def get_train_dir(self):
         return self.conf["train_dir"]
     def get_train_old_dir(self):
